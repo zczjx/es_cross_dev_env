fb81b048181de38aff5f102d13438a8dddf008b3
Clarence.Zhou <zhou_chenz@163.com>
2020-05-21 00:15:57 +0800

    update docker build with params

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-docker build -t arm_linux_env:18.04 -f Dockerfile.ubuntu_18_04 .
+if [ $# -lt 1 ]; then
+echo "usage: ./docker_build.sh [arm | armv8l | aarch64]"
+exit 1
+fi
+arch=$1
+
+if [ $arch == "arm" ]; then
+toolchain=gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar
+elif [ $arch == "armv8l" ]; then
+toolchain=gcc-linaro-7.5.0-2019.12-x86_64_armv8l-linux-gnueabihf.tar
+elif [ $arch == "aarch64" ]; then
+toolchain=gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar
+else
+echo "arch: $arch is not match the toolchain"
+exit 1
+fi
+echo "toolchain: $toolchain"
+docker build -t ${arch}_host_ubuntu:18.04 \
+--build-arg arch=${arch} \
+--build-arg toolchain=${toolchain} \
+-f Dockerfile.ubuntu_18_04 .
--- docker_run.sh
+++ docker_run.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+echo "usage: ./docker_run.sh [arm | armv8l | aarch64]"
+exit 1
+fi
+arch=$1
+
+if [ $arch == "arm" ]; then
+toolchain=gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar
+elif [ $arch == "armv8l" ]; then
+toolchain=gcc-linaro-7.5.0-2019.12-x86_64_armv8l-linux-gnueabihf.tar
+elif [ $arch == "aarch64" ]; then
+toolchain=gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar
+else
+echo "arch: $arch is not match the toolchain"
+exit 1
+fi
+
 username=git
 docker run -it \
 -v /dev:/dev \
 -v /home/${USER}/github/nfs/:/workspace \
 -v /home/${USER}/github/es_cross_dev_env/3rd-pkg:/home/${username}/3rd-pkg \
---privileged arm_linux_env:18.04
+--privileged ${arch}_host_ubuntu:18.04
