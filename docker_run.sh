5e12a7023cf64ab7c3ef48680eeb4dfcdee2c179
Clarence.Zhou <zhou_chenz@163.com>
2020-05-20 15:17:05 +0800

    fix user issue in script

--- docker_run.sh
+++ docker_run.sh
@@ -2,6 +2,6 @@
 username=git
 docker run -it \
 -v /dev:/dev \
--v /home/zcz/github/nfs/:/workspace \
--v /home/zcz/github/es_cross_dev_env/3rd-pkg:/home/${username}/3rd-pkg \
+-v /home/${USER}/github/nfs/:/workspace \
+-v /home/${USER}/github/es_cross_dev_env/3rd-pkg:/home/${username}/3rd-pkg \
 --privileged arm_linux_env:18.04
