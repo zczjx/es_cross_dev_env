69a8fb247351b06c5d7e000bf8f3109225fbd55a
Clarence.Zhou <zhou_chenz@163.com>
2020-05-21 18:16:04 +0800

    add toolchain and cmake

--- docker_build.sh
+++ docker_build.sh
@@ -7,10 +7,13 @@ arch=$1
 
 if [ $arch == "arm" ]; then
 toolchain=gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar
+toolchain_dir=gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf
 elif [ $arch == "armv8l" ]; then
 toolchain=gcc-linaro-7.5.0-2019.12-x86_64_armv8l-linux-gnueabihf.tar
+toolchain_dir=gcc-linaro-7.5.0-2019.12-x86_64_armv8l-linux-gnueabihf
 elif [ $arch == "aarch64" ]; then
 toolchain=gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar
+toolchain_dir=gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu
 else
 echo "arch: $arch is not match the toolchain"
 exit 1
@@ -19,4 +22,5 @@ echo "toolchain: $toolchain"
 docker build -t ${arch}_host_ubuntu:18.04 \
 --build-arg arch=${arch} \
 --build-arg toolchain=${toolchain} \
+--build-arg toolchain_dir=${toolchain_dir} \
 -f Dockerfile.ubuntu_18_04 .
